67df1d7dfbfeb726a21ee38d38cd58173499aea9
Matthias Springer <me@m-sp.org>
2025-06-22 09:21:22 +0000

    WIP

--- mlir/lib/Conversion/MemRefToLLVM/MemRefToLLVM.cpp
+++ mlir/lib/Conversion/MemRefToLLVM/MemRefToLLVM.cpp
@@ -2050,6 +2050,7 @@ struct FinalizeMemRefToLLVMConversionPass
                                                    &symbolTables);
     LLVMConversionTarget target(getContext());
     target.addLegalOp<func::FuncOp>();
+    target.addLegalOp<arith::ConstantOp>();
     if (failed(applyPartialConversion(op, target, std::move(patterns))))
       signalPassFailure();
   }
